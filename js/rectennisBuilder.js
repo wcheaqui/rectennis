0ba0a4937a4ccb33c83e434beb76f916cf26d7df
William Cheaqui <willcheaqui@yahoo.com>
2016-05-03 11:22:28 -0700

    add image slider

--- js/rectennisBuilder.js
+++ js/rectennisBuilder.js
@@ -1,34 +1,38 @@
-var counter = 0, // to keep track of current slide
-    $items = $('.slideshow figure'), // a collection of all of the slides, caching for performance
-    numItems = $items.length; // total number of slides
+var imagecount = 1;
+var total = 5;
 
-// this function is what cycles the slides, showing the next or previous slide and hiding all the others
-var showCurrent = function(){
-    var itemToShow = Math.abs(counter%numItems);// uses remainder (aka modulo) operator to get the actual index of the element to show
+function slide(x) {
+  var image = document.getElementById('img');
+  imagecount = imagecount + x;
+  if(imagecount > total){
+    imagecount = 1;}
 
-  $items.removeClass('show'); // remove .show from whichever element currently has it
-  $items.eq(itemToShow).addClass('show');
-};
+  if(imagecount < 1){
+    imagecount = total;}
 
-// add click events to prev & next buttons
-$('.next').on('click', function(){
-    counter++;
-    showCurrent();
-});
-$('.prev').on('click', function(){
-    counter--;
-    showCurrent();
-});
+  image.src = "images/img"+ imagecount +".jpg";
+}
 
-// if touch events are supported then add swipe interactions using TouchSwipe https://github.com/mattbryson/TouchSwipe-Jquery-Plugin
+window.setInterval(function slideA() {
+  var image = document.getElementById('img');
+  imagecount = imagecount + 1;
+  if(imagecount > total){
+    imagecount = 1;
+  }
+  if(imagecount < 1){
+    imagecount = total;
+  }
+  image.src = "images/img"+ imagecount +".jpg";
+}, 5000);
+
 if('ontouchstart' in window){
-  $('.slideshow').swipe({
+  $('#container').swipe({
     swipeLeft:function() {
-      counter++;
+      imagecount++;
       showCurrent();
     },
     swipeRight:function() {
-      counter--;
+      imagecount--;
       showCurrent();
     }
   });
